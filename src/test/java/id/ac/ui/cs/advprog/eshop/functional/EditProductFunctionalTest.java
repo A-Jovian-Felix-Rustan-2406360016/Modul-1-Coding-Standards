c93f13ab15b4e081658030e4795e0675b159270a
Jovian12 <infernum212@gmail.com>
2026-02-10 15:39:43 +0700

    Add web driver wait for edit test

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/EditProductFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/EditProductFunctionalTest.java
@@ -7,9 +7,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -23,6 +27,7 @@ class EditProductFunctionalTest {
 
     @Value("${app.baseUrl:http://localhost}")
     private String testBaseUrl;
+
     private String baseUrl;
 
     @BeforeEach
@@ -41,8 +46,9 @@ class EditProductFunctionalTest {
     void editProduct_isSuccessful(ChromeDriver driver) {
         String originalName = "Tes Sebelum Edit";
         createProductForEdit(driver, originalName);
-        driver.findElement(By.xpath("//td[contains(text(), '" + originalName + "')]/following-sibling::td/a[contains(@class, 'btn-warning')]")).click();
-        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement editButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//td[contains(text(), '" + originalName + "')]/following-sibling::td/a[contains(@class, 'btn-warning')]")));editButton.click();
+        WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameInput")));
         nameInput.clear();
         String newName = "Tes Sesudah Edit";
         nameInput.sendKeys(newName);
@@ -50,8 +56,9 @@ class EditProductFunctionalTest {
         quantityInput.clear();
         quantityInput.sendKeys("50");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
+        wait.until(ExpectedConditions.urlContains("/product/list"));
         String pageSource = driver.getPageSource();
-        assertTrue(pageSource.contains(newName));
-        assertFalse(pageSource.contains(originalName));
+        assertTrue(pageSource.contains(newName), "Nama produk baru tidak ditemukan!");
+        assertFalse(pageSource.contains(originalName), "Nama produk lama masih ada!");
     }
 }
